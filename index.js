df0c6889677e7efca4dc6623173114d43d78666e
Jarreth Santos <santos.jarreth@gmail.com>
2024-02-14 11:36:24 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+
+document.getElementById('main').remove();
+
+
+const newHeader = document.createElement('h1');
+
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'Jarreth Santos is the champion';
+
+
+document.body.appendChild(newHeader);
